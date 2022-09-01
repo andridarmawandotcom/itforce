7359a0293852934865526e32837cc9191f29fd90
andrinda <andri.drmawan@gmail.com>
2022-09-01 16:49:35 +0700

    gas

---
+++ app/Http/Controllers/ImsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImsController extends Controller
+{
+
+    public function cbInit()
+    {
+
+        # START CONFIGURATION DO NOT REMOVE THIS LINE
+        $this->title_field = "IMS";
+        $this->limit = "10";
+        $this->orderby = "id,asc";
+        $this->global_privilege = false;
+        $this->button_table_action = true;
+        $this->button_bulk_action = true;
+        $this->button_action_style = "button_icon";
+        $this->button_add = true;
+        $this->button_edit = true;
+        $this->button_delete = true;
+        $this->button_detail = true;
+        $this->button_show = true;
+        $this->button_filter = true;
+        $this->button_import = false;
+        $this->button_export = false;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $this->title_field = "IMS";
+        return view('ims.index');
+        // return view('ims.index', [
+        //     'user' => User::findOrFail($id)
+        // ]);
+    }
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
